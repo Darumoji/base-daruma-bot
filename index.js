4d5956ef51d7eaa89a49760b08291c74cd96f9b7
jermmdev <jermmdev@gmail.com>
2019-09-10 23:12:27 -0600

    now using trigger api

--- index.js
+++ index.js
@@ -1,169 +1,126 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
-const config = require("./config.json");
+let fs = require('fs')
+const Discord = require("discord.js")
+const client = new Discord.Client()
+const triggers = require("./triggers.json")
+const token = require("./token.json").token
 
+let funcMap = { 
+    "say": (targetParams)=>{
+        let channel = targetParams[0]
+        let content = targetParams[1]
+        console.log('content', content)
+        channel.send(content)
+    },
+    "hug": (targetParams)=>{
+        let channel = targetParams[0]
+        let user = targetParams[1]
+        channel.send(`<(^.^)> @${user}`)
+    },
+    "react": (targetParams)=>{
+        let message = targetParams[0]
+        let emojiName = targetParams[1]
+        console.log('emojiname', emojiName)
+        let emojires = client.emojis.find(emoji => emoji.name === emojiName)
+        if(emojires) message.react(emojires)
+    }
+}
 
 client.on("ready", () => {
-    console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
-    let dlounge = client.guilds.find(guild => guild.name==="DarumaLounge")
-    // let purgch = dlounge.channels.find(channel => channel.name === "purgatory")
-
-    // purgch.fetchMessages().then(fetched=>{
-    //     purgch.bulkDelete(fetched).catch(error => purgch.send([`Couldn't delete messages because of: ${error}`]));
-    // })
-    
-     
-    // purgch.send(["Welcome to purgatory, *Haha!  Hoho!*"
-    //     ,"Speak to receive a trait.  Choose wisely, for you can only move in one direction."
-    //     ,""
-    //     ,"**\"halo\" (G)**: Join heaven as a precious angel where only happy thoughts are allowed."
-    //     ,"   (Must not have *soul* or *horns*.)"
-    //     ,"**\"soul\" (PG-13)**: Join the common folk in the land of lame jokes."
-    //     ,"   (Must not have *horns* unless you also have a *halo*.)"
-    //     ,"**\"horns\" (MA)**: Join the grown-ups in the land of degenerates."
-    // ])
-    client.user.setActivity(`Serving ${client.guilds.size} servers`);
-
+    client.user.setActivity(`Haha!  Hoho!`);
     client.on("message", async message => {
-        if (message.author.bot) return;
-        if (message.content.indexOf('#19')>-1){
-            return message.react(`😏`)
+        if(message.author.bot) return
+       for(let trig in triggers){
+        let trigger = triggers[trig]
+        if(!trigger.message || !trigger.target) continue
+        if(trigger.message.reaction) continue
+        let msgParams = false
+        let msgOrMatch = false
+        for(let msgInd in trigger.message){
+            let msgAndConditions = trigger.message[msgInd]
+            let msgAndMatch = true
+            for(let msgAndInd in msgAndConditions){
+                let msgTrigger = msgAndConditions[msgAndInd]
+                if(msgTrigger.text){
+                    let matchRegex = RegExp(msgTrigger.text);
+                    let msgMatches = message.content.match(matchRegex)
+                    if(msgMatches === null) msgAndMatch = false
+                    else{
+                        msgParams = msgMatches
+                    }
+                }
+                if(msgTrigger.emoji && !(message.content.indexOf(`${client.emojis.find(emoji => emoji.name === msgTrigger.emoji)}`)>-1)) msgAndMatch = false
+                if(msgTrigger.user && !(message.author.tag === msgTrigger.user)) msgAndMatch = false
+            }
+            if(msgAndMatch) msgOrMatch = true
         }
-    
-        // if (message.channel.name === "purgatory") {
-        //     message.delete().catch(O_o => {
-        //         console.log('deletefail', O_o)
-        //     });
-            // let welcomeMessageId = false
-            // if (message.content === "halo") {
-            //     if (message.member.roles.some(r => ['soul', 'horns'].includes(r.name)))
-            //         message.reply("Sorry, you can't have a halo!");
-            //     var role = message.guild.roles.find(role => role.name === "halo");
-            //     message.member.addRole(role).then(m => {
-            //         m.reply("Welcome, sweet angel.").then(msg=>{msg.delete(5000)})
-            //     }).catch(e => {
-            //         if(welcomeMessageId) 
-            //         message.reply("Failed to gain halo.").then(msg=>{msg.delete(5000)})
-            //     });;
-            // }
-            // if (message.content === "soul") {
-            //     if (message.member.roles.some(r => ['horns'].includes(r.name)))
-            //         if(!message.member.roles.some(r => ['halo'].includes(r.name)))
-            //             message.reply("Sorry, you can't have a soul.").then(msg=>{msg.delete(5000)});
-            //     message.member.addRole(message.guild.roles.find(role => role.name === "soul")).then(m => {
-            //         m.reply("Sup.").then(msg=>{msg.delete(5000)})
-            //     }).catch(e => {
-            //         message.reply("Failed to gain soul.").then(msg=>{msg.delete(5000)})
-            //     });
-            // }
-            // if (message.content === "horns") {
-            //     message.member.addRole(message.guild.roles.find(role => role.name === "horns")).then(m => {
-            //         m.reply("Welcome to the jungle, baby!").then(msg=>{msg.delete(5000)})
-            //     }).catch(e => {
-            //         message.reply("Failed to gain horns.").then(msg=>{msg.delete(5000)})
-            //     });;
-            // }
-        // }
-    
-        const prereg = new RegExp(config.prefix);
-        let messageMatch = message.content.match(prereg);
-        if (!messageMatch || messageMatch.length == 0) return
-        const args = message.content.slice(messageMatch[0]).trim().split(/ +/g);
-        args.shift();
-        const command = args.shift().toLowerCase();
-    
-        if (command === "ping") {
-            const m = await message.channel.send("Ping?");
-            m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+        if(!msgOrMatch) continue
+        if(trigger.authorization){
+            let authOrMatch = false
+            for(let authInd in trigger.authorization){
+                let authAndConditions = trigger.authorization[authInd]
+                let authAndMatch = true
+                for(let authAndInd in authAndConditions){
+                    let auth = authAndConditions[authAndInd]
+                    if(auth.guild && message.guild.name!==auth.guild) authAndMatch = false
+                    if(auth.role && !message.member.roles.some(r => auth.role === r.name)) authAndMatch = false
+                }
+                if(authAndMatch) authOrMatch = true
+            }
+            if(!authOrMatch) continue 
         }
-    
-        if (command === "say") {
-            //console.log('saycommand', message.author.id)
-            //if(message.author.id !== "555509356637061136") return
-            const sayMessage = args.join(" ");
-            if(sayMessage.length > 200) return message.channel.send("lol tl;dr")
-            message.delete().catch(O_o => {});
-            message.channel.send(sayMessage);
+        let targetSplit = trigger.target.split('/')
+        if(targetSplit.length > 0){
+            let targetFunc = false
+            let targetParams = []
+            for(let splitInd in targetSplit){
+                let targetParam = targetSplit[splitInd]
+                if(splitInd<1){
+                    targetFunc = funcMap[targetParam]
+                    if(!targetFunc) return console.log('invalid target func', message.member.nickname || message.author.username, message.content)
+                    continue
+                }
+                if(targetParam==='$guild'){
+                    targetParams.push(message.guild)
+                    continue
+                }
+                if(targetParam==='$message'){
+                    targetParams.push(message)
+                    continue
+                }
+                if(targetParam==='$user'){
+                    targetParams.push(message.user)
+                    continue
+                }
+                if(targetParam==='$member'){
+                    targetParams.push(message.member)
+                    continue
+                }
+                if(targetParam==='$channel'){
+                    targetParams.push(message.channel)
+                    continue
+                }
+                let paramMatch = targetParam.match(/\$([0-9]+)/)
+                console.log('parammatch', paramMatch, targetParam)
+                if(paramMatch !== null && paramMatch.length > 0){
+                    let msgParamIndex = parseInt(paramMatch[1])
+                    if(!msgParams.length > msgParamIndex) return message.channel.send(`Invalid Target Parameters Provided, ${message.member.nickname}`)
+                    targetParams.push(msgParams[msgParamIndex])
+                    continue
+                }
+                targetParams.push(targetParam)
+            }
+            targetFunc(targetParams)
         }
-
-        if (command === "ascend") {
-            if (!message.member.roles.some(r => config.modRoles.includes(r.name)))
-                return message.reply("Sorry, you don't have permissions to use this!");
-            const mbtag = args.shift()
-            let halotarget = dlounge.members.find(mb=>mb.user.tag === mbtag)
-            if(halotarget) halotarget.addRole(message.guild.roles.find(role => role.name === "halo")).then(m => {
-                message.reply(`${halotarget.user.username} has been granted a halo.`)
-            }).catch(e => {
-                console.log('ascend err', e)
-                message.reply(`${halotarget.user.username} could not be ascended.`).then(msg=>{msg.delete(5000)})
-            })
-
-        }
-        if (command === "ensoul") {
-            if (!message.member.roles.some(r => config.modRoles.includes(r.name)))
-                return message.reply("Sorry, you don't have permissions to use this!");
-            const member = args.shift()
-            let soulTarget = dlounge.members.find(mb=>mb.id === member)
-            soulTarget.addRole(message.guild.roles.find(role => role.name === "soul")).then(m => {
-                message.reply(`${hornTarget.user.username} has been granted a soul.`).then(msg=>{
-                    soulTarget.removeRole(dlounge.roles.find(role => role.name === 'halo')).then(ms => {
-                        message.reply(`${hornTarget.user.username} has been banished from heaven.`)
-                    }).catch(e => {
-                        message.reply(`${hornTarget.user.username} had no halo.`).then(msg=>{msg.delete(5000)})
-                    })
-                })
-            }).catch(e => {
-                message.reply(`${hornTarget.user.username} could not be ensouled.`).then(msg=>{msg.delete(5000)})
-            })
-
-        }
-        if (command === "damn") {
-            if (!message.member.roles.some(r => config.modRoles.includes(r.name)))
-                return message.reply("Sorry, you don't have permissions to use this!");
-            const member = args.shift()
-            let hornTarget = dlounge.members.find(mb=>mb.id === member)
-            hornTarget.addRole(message.guild.roles.find(role => role.name === "horns")).then(x => {
-                message.reply(`${hornTarget.user.username} has grown horns.`).then(xxxx=>{
-                    hornTarget.removeRole(dlounge.roles.find(role => role.name === 'halo')).then(xx => {
-                        message.reply(`${hornTarget.user.username} has been banished from heaven.`)
-                        hornTarget.removeRole(dlounge.roles.find(role => role.name === 'soul')).then(xxx => {
-                            message.reply(`${hornTarget.user.username} has fallen from earth.`)
-                        }).catch(e => {
-                            message.reply(`${hornTarget.user.username} had no soul.`).then(msgg=>{msgg.delete(5000)})
-                        })
-                    }).catch(e => {
-                        message.reply(`${hornTarget.user.username} had no halo.`).then(msggg=>{msggg.delete(5000)})
-                    })
-                })
-            }).catch(e => {
-                message.reply(`${hornTarget.user.username} could not be damned.`).then(msgggg=>{msgggg.delete(5000)})
-            })
-        }
-    
-        if (command === "purge") {
-            if (!message.member.roles.some(r => config.modRoles.includes(r.name)) && message.author.id !== "555509356637061136")
-                return message.reply("Sorry, you don't have permissions to use this!");
-    
-            const deleteCount = parseInt(args[0], 10);
-            if (!deleteCount || deleteCount < 2 || deleteCount > 100)
-                return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
-            const fetched = await message.channel.fetchMessages({
-                limit: deleteCount
-            });
-            message.channel.bulkDelete(fetched)
-                .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
-        }
-    });
-});
-
+       }
+    })
+    console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`)
+})
 client.on("guildCreate", guild => {
     console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
     client.user.setActivity(`Serving ${client.guilds.size} servers`);
-});
-
+})
 client.on("guildDelete", guild => {
     console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
     client.user.setActivity(`Serving ${client.guilds.size} servers`);
-});
-
-client.login(config.token);
+})
+client.login(token)
